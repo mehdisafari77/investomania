6537828b0cbc747ea1305993e9c6993bbbfb39a0
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-11-01 11:46:03 -0700

    add the user model attributes

--- models/User.js
+++ models/User.js
@@ -4,4 +4,25 @@ const bcrypt = require('bcrypt')
 
 class User extends Model {checkPassword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
-  }}
+  }
+}
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [5]
+        }
+    }
+},
