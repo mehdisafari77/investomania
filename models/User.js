2e4f5261095f73b0ecfbf8da0f32c1c1d0cd1437
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-11-01 11:47:01 -0700

    add hooks and export module in user model

--- models/User.js
+++ models/User.js
@@ -26,3 +26,25 @@ User.init({
         }
     }
 },
+
+{
+    hooks: {
+        // set up beforeCreate lifecycle "hook" functionality
+        beforeCreate: async (newUserData) => {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        },
+        beforeUpdate: async (updatedUserData) => {
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          return updatedUserData;
+        }
+      },
+    sequelize, 
+    freezeTableName: true,
+    timestamps: false,
+    underscored: true,
+    modelName: 'user'
+}
+)
+
+module.exports = User
