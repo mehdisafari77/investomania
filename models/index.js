3b8b167efa834d9b9a5964c6b17d06f6bec9558e
Umera <umeramalek796@gmail.com>
2021-11-02 10:30:57 -0700

    added relationships

--- models/index.js
+++ models/index.js
@@ -2,18 +2,36 @@ const Backing = require('./Backing')
 const Project = require('./Project')
 const User = require('./User')
 
+User.hasMany(Project, {
+    foreignKey: 'project_id',
+    onDelete: 'CASCADE'
+})
+User.hasMany(Backing, {
+    foreignKey:'backing_id',
+    onDelete: 'CASCADE'
+})
+
 Project.belongsTo(User, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
 })
 
 Project.hasMany(User, {
-    foreignKey: 'post_id',
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+Project.hasMany(Backing, {
+    foreignKey:'backing_id',
     onDelete: 'CASCADE'
 })
 
 Backing.belongsTo(User, {
     foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+})
+
+Backing.belongsTo(Project, {
+    foreignKey:'project_id',
     onDelete: 'CASCADE'
 })
 
