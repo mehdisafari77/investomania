073706538867105c972c729abef974987548cfef
samikhawja <skhawja11@gmail.com>
2021-11-03 14:34:22 -0700

    reorganized dashboardJS

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,3 +1,36 @@
+let projectTitle;
+let projectDesc;
+let projectImg;
+let saveBtn;
+
+if(window.location.pathname === '/dashboard') {
+  projectTitle = document.querySelector('#project-title');
+  projectDesc = document.querySelector('#project-desc');
+  projectImg = document.querySelector('#project-img');
+  saveBtn = document.querySelector('#save-project');
+}
+
+const saveProject = (event) => {
+  event.preventDefault();
+  let projects = {
+    title: projectTitle.value.trim(),
+    description: projectDesc.value.trim(),
+    image: projectImg.value.trim(),
+  }
+  fetch('/api/projects', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify(projects)
+  })
+};
+
+if(window.location.pathname === '/dashboard') 
+{
+  saveBtn.addEventListener('click', saveProject);
+}
+
+// ---------------------------------------------------------
+
 // const newFormHandler = async (event) => {
 //   event.preventDefault();
 
@@ -22,34 +55,6 @@
 //   .addEventListener('click', delButtonHandler);
 // }
 
-let projectTitle;
-let projectDesc;
-let projectImg;
-let saveBtn;
-// let deleteBtn;
-
-if(window.location.pathname === '/dashboard') {
-  projectTitle = document.querySelector('#project-title');
-  projectDesc = document.querySelector('#project-desc');
-  projectImg = document.querySelector('#project-img');
-  saveBtn = document.querySelector('#save-project');
-  // deleteBtn = document.querySelector('.delete-project');
-  // console.log(deleteBtn)
-}
-
-const saveProject = (event) => {
-  event.preventDefault();
-  let projects = {
-    title: projectTitle.value.trim(),
-    description: projectDesc.value.trim(),
-    image: projectImg.value.trim(),
-  }
-  fetch('/api/projects', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json'},
-    body: JSON.stringify(projects)
-  })
-};
 
 // const deleteProject = (event) => {
 //   event.preventDefault();
@@ -79,10 +84,3 @@ const delButtonHandler = async (event) => {
 document
   .querySelector('.delete-project')
   .addEventListener('click', delButtonHandler);
-
-
-if(window.location.pathname === '/dashboard') 
-{
-  saveBtn.addEventListener('click', saveProject);
-  // deleteBtn.addEventListener('click', deleteProject);
-}
--- public/js/login.js
+++ public/js/login.js
@@ -21,7 +21,7 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#name-signup').value.trim();
+  const name = document.querySelector('#email-signup').value.trim();
   const username = document.querySelector('#username-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
