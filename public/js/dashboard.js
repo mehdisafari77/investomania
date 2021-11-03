dc9260d97c61e8a263ecd57781a2f06a7a8c7c93
samikhawja <skhawja11@gmail.com>
2021-11-03 10:13:29 -0700

    troubleshoot logo

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,20 +1,21 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#post-title').value.trim();
-  const description = document.querySelector('#post-desc').value.trim();
+  const name = document.querySelector('#project-name').value.trim();
+  const description = document.querySelector('#project-desc').value.trim();
+  const image = document.querySelector('#project-img').value.trim();
 
-  if (name && needed_funding && description) {
+  if (name && needed_funding && description && image) {
     const response = await fetch(`/api/posts`, {
       method: 'POST',
-      body: JSON.stringify({ name, description }),
+      body: JSON.stringify({ name, description, image }),
       headers: {
         'Content-Type': 'application/json',
       },
     });
 
     if (response.ok) {
-      document.location.replace('/post');
+      document.location.replace('/dashboard');
     } else {
       alert('Failed to create post');
     }
